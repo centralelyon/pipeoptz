dbfb3102c1533452d068885a6f2e18c2b6ff8ef0
Nicolas Pengov <61360444+pengov@users.noreply.github.com>
2025-09-17 15:42:03 +0200

    v0.1.3

--- src/pipeoptz/__init__.py
+++ src/pipeoptz/__init__.py
@@ -3,5 +3,5 @@ from .parameter import IntParameter, FloatParameter, BoolParameter, ChoiceParame
 from .pipeline import Pipeline
 from .optimizer import PipelineOptimizer
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = ["Node", "NodeIf", "NodeFor", "NodeWhile", "IntParameter", "FloatParameter", "BoolParameter", "ChoiceParameter", "MultiChoiceParameter", "Pipeline", "PipelineOptimizer"]
